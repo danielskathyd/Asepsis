4cd66529da740a0402278d64d6059dd1a176522f
Matthew Wang <matthew.wang.c@gmail.com>
2018-11-17 21:10:57 -0800

    change key

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 /* API KEY value for breezometer data*/
-const BREEZE_API_KEY = "b1db2002f7464d81a9707fb36213a0c3";
+const BREEZE_API_KEY = "353eda9f828f4edf91334ebd5f44ea18";
 
 /* location constants */
 const UCLA_LAT = 34.0689;
